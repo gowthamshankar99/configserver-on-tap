18eebcd052c71b7ba962eba1b95fd2872d0ba2a5
Gowtham Shankar <gowthamshankar@sgowthamsha-a01.vmware.com>
2022-05-29 17:19:47 -0400

    changes

--- src/main/java/com/example/configserver/GitAuthBindingsPropertiesProcessor.java
+++ src/main/java/com/example/configserver/GitAuthBindingsPropertiesProcessor.java
@@ -40,9 +40,9 @@ public class GitAuthBindingsPropertiesProcessor implements BindingsPropertiesPro
         properties.put("spring.cloud.config.server.git.username", secret.get("username"));
         properties.put("spring.cloud.config.server.git.password", secret.get("password"));
         properties.put("spring.cloud.config.server.git.skipSslValidation", secret.get("skipSslValidation"));
-        properties.put("spring.cloud.config.server.git.repos.development.pattern", "*/development");
+        properties.put("spring.cloud.config.server.git.repos.development.pattern", "{application}/development");
         properties.put("spring.cloud.config.server.git.repos.development.uri", "https://github.com/gowthamshankar99/spring-config-dev.git");
-        properties.put("spring.cloud.config.server.git.repos.staging.pattern", "*/staging");
+        properties.put("spring.cloud.config.server.git.repos.staging.pattern", "{application}/staging");
         properties.put("spring.cloud.config.server.git.repos.staging.uri", "https://github.com/gowthamshankar99/spring-config-prod.git");
         log.info("Mappedd spring.cloud.config.server.git.uri to {}", properties.get("spring.cloud.config.server.git.uri"));
     }
